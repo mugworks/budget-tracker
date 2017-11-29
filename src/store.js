7b001a7657e2b37b881f8c7cf7c09ba2ea196fbe
mugworks <greenwood.pdx@gmail.com>
2017-11-29 10:46:46 -0800

    add thunk middleware

--- src/store.js
+++ src/store.js
@@ -1,8 +1,14 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import categories from './categories/reducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__  && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const store = createStore(categories, composeEnhancers);
+const store = createStore(
+  categories, 
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+);
 
 export default store;
