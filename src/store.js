d8f81006f61f98bc365b2cf9b07fbee5c65fd0dd
mugworks <greenwood.pdx@gmail.com>
2017-11-29 10:32:55 -0800

    fix store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 import { createStore } from 'redux';
 import categories from './categories/reducer';
 
-const store = createStore(categories);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__  && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(categories, composeEnhancers);
 
 export default store;
