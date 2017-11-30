3d6bd4149dac86aa7b27811f087794fd95905d8a
mugworks <greenwood.pdx@gmail.com>
2017-11-29 19:57:10 -0800

    add load categories

--- src/categories/actions.js
+++ src/categories/actions.js
@@ -1,31 +1,43 @@
-import { CATEGORY_ADD, CATEGORY_UPDATE, CATEGORY_REMOVE, CATEGORY_LOAD } from './constants';
+import { CATEGORY_ADD, CATEGORY_UPDATE, CATEGORY_REMOVE, CATEGORY_LOAD, CATEGORY_ERROR } from './constants';
 import categoryApi from '../services/category-api.js';
 // import shortid from 'short-id';
 
-export function loadCategories(id) {
-  return async (dispatch, getState) => {
-    const { categories } = getState;
-    if(categories[id]) return;
-
-    const loaded = await categoryApi.get(id);
-    dispatch({
-      type: CATEGORY_LOAD,
-      payload: { categories: loaded }
-    });
+export function loadCategories() {
+  return async dispatch => {
+    try {
+      const categories = await categoryApi.get();
+      dispatch({
+        type: CATEGORY_LOAD,
+        payload: categories
+      });
+    }
+    catch(err) {
+      dispatch({
+        type: CATEGORY_ERROR,
+        payload: err
+      });
+    }
   };
 }
 
-// export function addCategory(category) {
-//   return async dispatch => {
-//     category._id = id;
-//     const saved = await categoryApi.add(category);
-//     category.timestamp = new Date();
-
-//   dispatch{
-//     type: CATEGORY_ADD,
-//     payload: saved
-//   };
-// }
+export function addCategory(category) {
+  return async dispatch => {
+    try {
+      const saved = await categoryApi.add(category);
+      // category.timestamp = new Date();
+      dispatch({
+        type: CATEGORY_ADD,
+        payload: saved
+      });
+    }
+    catch(err) {
+      dispatch({
+        type: CATEGORY_ERROR,
+        payload: err
+      });
+    }
+  };
+}
 
 export function updateCategory(category) {
   return{
