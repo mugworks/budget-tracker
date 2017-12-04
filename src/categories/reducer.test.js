ca470d5a9be2b8a0ba4cd00c8a04c86789b23a4b
mugworks <greenwood.pdx@gmail.com>
2017-12-04 12:11:09 -0800

    add reducer tests

---
+++ src/categories/reducer.test.js
@@ -0,0 +1,35 @@
+import reducer from './reducer';
+import { CATEGORY_ADD, CATEGORY_REMOVE, CATEGORY_UPDATE } from './constants';
+
+describe('categories reducer', () => {
+
+  it('initializes', () => {
+    const state = reducer(undefined, {});
+    expect(state).toEqual([]);
+  });
+
+  it('adds a category', () => {
+    const category = { name: 'animals', budget: '3' };
+    const state = reducer([], { type: CATEGORY_ADD, payload: category });
+    expect(state).toEqual([category]);
+  });
+
+  it('removes a category', () => {
+    const category = { _id: 123, name: 'animals', budget: '3' };
+    const state = reducer([category], { type: CATEGORY_REMOVE, payload: category._id });
+    expect(state).toEqual([]);
+  });
+
+  it('updates a category', () => {
+    const category = { _id: 123, name: 'animals', budget: '3' };
+    const state = reducer([category], { 
+      type: CATEGORY_UPDATE, 
+      payload: { _id: 123, name: 'birds', budget: '4' } 
+    });
+    expect(state).toEqual([
+      { ...category,  name: 'birds', budget: '4' } 
+    ]);
+  });
+
+  
+});
